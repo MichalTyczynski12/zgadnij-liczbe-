5ce461e92dc3be76c29f7fdd23a7c75281e9ab2d
MichalTyczynski12 <105392395+MichalTyczynski12@users.noreply.github.com>
2022-06-06 11:08:36 +0200

    Add files via upload

---
+++ zgadnij liczbe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Random rnd = new Random();
+		int liczbaSzukana = rnd.Next(1, 11); // liczba losowa od 1 do 10
+
+		while (true)
+		{
+			Console.Write("Wpisz liczbe: ");
+			int liczba = int.Parse(Console.ReadLine());
+			if (liczba == liczbaSzukana)
+            {
+				Console.Write("gratulacje zgadłeś liczbę");
+				break;
+			}
+            else if(liczba < liczbaSzukana)
+            {
+				Console.Write("Liczba szukana jest wieksza!\n");
+			}
+            else
+            {
+				Console.Write("Liczba szukana jest mniejsza!\n");
+			}
+
+
+		}
+
+	}
+}
